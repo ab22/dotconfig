a00067d8da2c545486b07b44166c1d9af958b34d
Abelardo Mendoza <abelardo22.9@gmail.com>
2024-02-23 15:35:07 -0600

    add nvim-tree

--- lua/ab22/autogroups.lua
+++ lua/ab22/autogroups.lua
@@ -18,3 +18,19 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     end,
     group = format_golang_grp,
 })
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+    callback = function(data)
+        local directory = vim.fn.isdirectory(data.file) == 1
+
+        if not directory then
+            return
+        end
+
+        -- Change to the directory
+        vim.cmd.cd(data.file)
+
+        -- Open the tree
+        require("nvim-tree.api").tree.open()
+    end
+})
--- lua/ab22/lazy/golang.lua
+++ lua/ab22/lazy/golang.lua
@@ -3,7 +3,7 @@ return {
     config = function()
         require("go").setup()
     end,
-    event = {"CmdlineEnter"},
-    ft = {"go", "gomod"},
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
     build = ':lua require("go.install").update_all_sync()'
 }
