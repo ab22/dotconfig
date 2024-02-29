59100203b227dc546dfba14cffd9c2dc76c8facb
Abelardo Mendoza <abelardo22.9@gmail.com>
2024-02-28 22:01:37 -0600

    customize status bar

--- lua/ab22/lazy/lualine.lua
+++ lua/ab22/lazy/lualine.lua
@@ -16,6 +16,27 @@ return {
                     },
                 },
             },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = {
+                    { 'filename', path = 1 }
+                },
+                lualine_c = { 'branch', 'diff', 'diagnostics' },
+                lualine_x = { 'location', { 'encoding', 'fileformat', 'filetype' },
+                    lualine_y = {},
+                    lualine_z = {},
+                },
+                inactive_sections = {
+                    lualine_a = { 'mode' },
+                    lualine_b = {
+                        { 'filename', path = 1 }
+                    },
+                    lualine_c = { 'branch', 'diff', 'diagnostics' },
+                    lualine_x = { 'location', 'encoding', 'fileformat', 'filetype' },
+                    lualine_y = {},
+                    lualine_z = {},
+                },
+            }
         }
     end
 }
