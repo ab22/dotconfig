841afd8a87c928f89aaedb030bd1d1072c9a3269
abe <abelardo22.9@gmail.com>
2024-03-17 20:19:03 -0600

    add golang test keymaps

--- nvim/lua/ab22/lazy/golang.lua
+++ nvim/lua/ab22/lazy/golang.lua
@@ -2,6 +2,13 @@ return {
     "ray-x/go.nvim",
     config = function()
         require("go").setup()
+
+        vim.keymap.set('n', '<leader>gtn', function()
+            vim.cmd('GoTest -vn')
+        end)
+        vim.keymap.set('n', '<leader>gta', function()
+            vim.cmd('GoTest -v')
+        end)
     end,
     event = { "CmdlineEnter" },
     ft = { "go", "gomod" },
