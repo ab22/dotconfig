44edc1d51901cc3dcf9a36033cc839b11f8b71d9
abe <abelardo22.9@gmail.com>
2024-05-21 20:53:16 -0600

    disable completion on comments

--- nvim/lua/ab22/lazy/lsp.lua
+++ nvim/lua/ab22/lazy/lsp.lua
@@ -118,6 +118,16 @@ return {
                     return kind
                 end,
             },
+            enabled = function()
+                local context = require 'cmp.config.context'
+
+                -- disable completion in comments.
+                if vim.api.nvim_get_mode().mode == 'c' then
+                    return true
+                end
+
+                return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+            end
         })
 
         require("lsp_signature").setup({
